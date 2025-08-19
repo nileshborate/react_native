acae5e996a17043620c4b58571ac9bb2c5870cbb
nileshborate <nileshhanumantborate@gmail.com>
2025-08-19 22:35:50 +0530

    inline styling

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -2,8 +2,10 @@ import { Text, View } from 'react-native';
 
 function App() {
   return (
-    <View>
-      <Text>Hello World!!!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'red', fontSize: 34, fontWeight: 'bold' }}>
+        Hello World!!!
+      </Text>
     </View>
   );
 }
