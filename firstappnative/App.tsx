9b936f1af052a14bed8023012378acb822fedefd
nileshborate <nileshhanumantborate@gmail.com>
2025-08-19 22:40:29 +0530

    using stylesheet to create style

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,12 +1,24 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: 'red', fontSize: 34, fontWeight: 'bold' }}>
-        Hello World!!!
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>👋 Welcome</Text>
+      <Text style={styles.name}>Name: Nilesh Borate</Text>
+      <Text style={styles.course}>Course: React Native Basics</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: { fontSize: 30, fontWeight: 'bold', color: 'blue' },
+  name: { fontSize: 20, marginTop: 10 },
+  course: { fontSize: 18, marginTop: 5, color: 'green' },
+});
+
 export default App;
