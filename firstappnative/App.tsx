3fc308a76082e794ae3cd9128dff6aa4dea55a3d
nileshborate <nileshhanumantborate@gmail.com>
2025-08-20 22:24:25 +0530

    useEffect example

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const [name, setName] = useState('');
+  useEffect(() => {
+    console.log('App loaded');
+  }, []);
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Enter Name" onChangeText={setName} />
-      <Text style={{ fontSize: 20 }}>Your Name : {name}</Text>
+      <Text style={{ fontSize: 20 }}>Check Console!!!</Text>
     </View>
   );
 }
