5d4d0746443707d3fb3ac2e43119e53dca3cc5e1
nileshborate <nileshhanumantborate@gmail.com>
2025-08-20 21:58:07 +0530

    arrow function component

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,31 +1,14 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
+  const MyName = () => <Text>My Name is Nilesh</Text>;
   return (
-    <View style={styles.card}>
-      <Image
-        source={{ uri: 'https://randomuser.me/api/portraits/men/75.jpg' }}
-        style={styles.image}
-      />
-      <Text style={styles.name}>John Doe</Text>
-      <Text style={styles.role}>Mobile App Developer</Text>
-      <Text style={styles.contact}>📧 john@example.com</Text>
-      <Text style={styles.contact}>📞 +91 9876543210</Text>
+    <View>
+      <MyName />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#e3f2fd',
-  },
-  image: { width: 120, height: 120, borderRadius: 60, marginBottom: 15 },
-  name: { fontSize: 24, fontWeight: 'bold' },
-  role: { fontSize: 18, color: 'gray', marginBottom: 10 },
-  contact: { fontSize: 16 },
-});
+const styles = StyleSheet.create({});
 
 export default App;
