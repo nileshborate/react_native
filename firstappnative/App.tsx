a8d42ebfb3eeed945f0b9ad6b919ffed8424f386
nileshborate <nileshhanumantborate@gmail.com>
2025-08-21 22:04:07 +0530

    scrollview

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,86 +1,56 @@
-import { useState } from 'react';
-import {
-  Button,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-
-const Btn = ({ label }) => (
-  <Pressable
-    onPress={() => console.log('clicked')}
-    style={({ pressed }) => [
-      styles.btn,
-      {
-        opacity: pressed ? 0.7 : 1,
-        transform: [{ scale: pressed ? 0.98 : 1 }],
-      },
-    ]}
-  >
-    <Text style={styles.btnText}>{label}</Text>
-  </Pressable>
-);
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
+    <ScrollView contentContainerStyle={styles.content}>
+      <Text style={styles.h1}>React Native Basics</Text>
+      <Image
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        style={styles.logo}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Btn label="Login" />
-      <Btn label="Signup" />
-    </View>
+      <Text style={styles.p}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        placeat veritatis iure non deserunt provident eveniet dignissimos
+        voluptatibus? Temporibus commodi in ab, aut eum nesciunt rem vitae
+        beatae! In laborum facere eum deserunt vero non esse explicabo commodi,
+        nisi alias corrupti dolorem voluptates animi eius, eos quibusdam
+        molestiae. Ipsam suscipit impedit libero iure quidem adipisci pariatur,
+        id dolores asperiores, veniam quasi corporis perferendis itaque
+        consequatur magni quisquam alias odio enim harum? Dolorem explicabo
+        illum velit? Atque harum commodi eaque eos ducimus exercitationem id
+        obcaecati sed esse non ab neque asperiores dolorem nihil iste libero
+        odio animi, magni quod, impedit, minima consequuntur. Neque laudantium
+        possimus mollitia ex cupiditate odit aliquam, perspiciatis commodi
+        delectus et quibusdam officiis, ut quia vel fugiat aliquid non! Ad
+        distinctio nemo facilis deleniti porro natus magni maxime praesentium,
+        cupiditate sed nobis, incidunt eius quaerat, odit earum. Veniam
+        quibusdam quos dolores dolor doloremque minus alias, sapiente aut iste
+        voluptate nostrum ullam inventore voluptas. Earum aliquid soluta
+        officiis cupiditate mollitia ipsam ad consectetur, pariatur deleniti
+        quam eum nobis dicta quidem vero placeat excepturi aut architecto
+        delectus natus quae molestias nesciunt deserunt debitis tenetur. Tempora
+        quasi ipsum, veritatis ab earum odit maiores nesciunt illo. Tempore
+        corrupti quo odio blanditiis harum? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Voluptatum, quasi quis soluta quibusdam
+        distinctio a commodi ut maxime eum in cumque cum ex, deserunt eligendi,
+        necessitatibus harum nam architecto? Adipisci debitis qui, dolorum,
+        suscipit voluptatem eveniet, odit quod at quia error quaerat vero
+        nostrum doloribus nemo voluptatum inventore officiis nesciunt laudantium
+        accusamus ab ducimus beatae optio consequuntur porro. Sunt magnam
+        accusantium sequi repudiandae, cupiditate ut impedit exercitationem in
+        nostrum ab dolor rerum, dolorem laboriosam consequuntur dicta. Atque
+        incidunt quod, repellendus quae quo sequi ut, cupiditate nesciunt velit
+        illum facere eveniet perspiciatis dolorum provident veniam at rem nulla
+        accusantium magnam sint.
+      </Text>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 18,
-  },
-  btn: {
-    backgroundColor: '#1976D2',
-    paddingVertical: 12,
-    paddingHorizontal: 18,
-    borderRadius: 12,
-    margin: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  btnText: {
-    color: '#fff',
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: '700',
-    marginBottom: 12,
-    textAlign: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#DDD',
-    borderRadius: 10,
-    padding: 12,
-    marginBottom: 10,
-    backgroundColor: '#fff',
-  },
+  content: { padding: 18 },
+  h1: { fontSize: 28, fontWeight: '800', marginBottom: 8 },
+  logo: { width: 64, height: 64, marginVertical: 12 },
+  p: { fontSize: 16, lineHeight: 22, color: '#333' },
 });
 export default App;
