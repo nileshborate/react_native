4986b5f5bac419f77ccbafe7c25ed643b9f8e5c5
nileshborate <nileshhanumantborate@gmail.com>
2025-08-25 22:16:11 +0530

    multiple actions by props using function

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-const Increment = ({ increase }) => <Button title="Add" onPress={increase} />;
+const Actions = ({ increment, reset }) => (
+  <View style={{ flexDirection: 'row', gap: 10, marginTop: 10 }}>
+    <Button title="Add" onPress={increment} />
+    <Button title="Reset" onPress={reset} />
+  </View>
+);
 
 function App() {
   const [total, setTotal] = useState(0);
@@ -9,7 +14,10 @@ function App() {
   return (
     <View style={{ padding: 20 }}>
       <Text>Total : {total}</Text>
-      <Increment increase={() => setTotal(total + 1)} />
+      <Actions
+        increment={() => setTotal(total + 1)}
+        reset={() => setTotal(0)}
+      />
     </View>
   );
 }
