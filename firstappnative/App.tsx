d998ccdd26321ac8cbee6feb3720bb0679d4d452
nileshborate <nileshhanumantborate@gmail.com>
2025-09-02 21:54:13 +0530

    simple flat list

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,26 +1,16 @@
 import { useRef, useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const [liked, setLiked] = useState(false);
-  const lastTap = useRef(0);
-  const onTap = () => {
-    const now = Date.now();
-    if (now - lastTap.current < 300) {
-      setLiked(!liked);
-    }
-    lastTap.current = now;
-  };
-
+  const data = ['Apple', 'Banana', 'Cherry', 'Dates'];
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Pressable
-        style={{ padding: 24, backgroundColor: '#E1F5FE', borderRadius: 12 }}
-        onPress={onTap}
-      >
-        <Text style={{ fontSize: 28 }}>{liked ? '❤️' : '🤍'}</Text>
-      </Pressable>
-      <Text style={{ marginTop: 8 }}>Double-tap the card</Text>
+    <View style={{ paddingTop: 60 }}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return <Text style={{ padding: 12, fontSize: 16 }}>{item}</Text>;
+        }}
+      />
     </View>
   );
 }
