84e1d06b35be4fab3337678c4158b10b73ca4591
nileshborate <nileshhanumantborate@gmail.com>
2025-09-07 10:24:04 +0530

    debounse search - type - wait - fetch

--- firstappnative/App.tsx
+++ firstappnative/App.tsx
@@ -1,56 +1,58 @@
 import { useEffect, useRef, useState } from 'react';
-import { ActivityIndicator, FlatList, Text } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
-const PAGE_SIZE = 25;
 function App() {
   const [data, setData] = useState([]);
-  const [loadingMore, setLoadingMore] = useState(false);
-
-  const [page, setPage] = useState(1);
-  const canLoad = useRef(true);
-
-  const fetchPage = async p => {
-    const resp = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_limit=${PAGE_SIZE}&_page=${p}`,
-    );
-    const rows = await resp.json();
-    console.log('Response = ', rows);
-    if (rows.length < PAGE_SIZE) {
-      canLoad.current = false;
-    }
-    setData(prev => [...prev, ...rows]);
-  };
+  const [q, setQ] = useState('');
 
   useEffect(() => {
-    fetchPage(1);
-  }, []);
+    const fetchPage = setTimeout(async () => {
+      if (!q) {
+        setData([]);
+        return;
+      }
+      const resp = await fetch(`https://jsonplaceholder.typicode.com/users`);
+      const all = await resp.json();
+      console.log('Response = ', all);
+      const filtered = all.filter(row =>
+        row.name.toLowerCase().includes(q.toLowerCase()),
+      );
+      console.log('filtered = ', filtered);
 
-  const onEnd = async () => {
-    if (loadingMore || !canLoad.current) return;
-
-    setLoadingMore(true);
-    const next = page + 1;
-    await fetchPage(next);
-    setPage(next);
-    setLoadingMore(false);
-  };
+      setData(filtered);
+    }, 500);
+    return () => clearTimeout(fetchPage);
+  }, [q]);
 
   return (
-    <FlatList
-      style={{ marginTop: 50 }}
-      data={data}
-      keyExtractor={x => String(x.id)}
-      renderItem={({ item }) => (
-        <Text style={{ padding: 12 }}>
-          {item.id} : {item.title}
-        </Text>
-      )}
-      onEndReached={onEnd}
-      ListFooterComponent={
-        loadingMore ? <ActivityIndicator style={{ margin: 12 }} /> : null
-      }
-      onEndReachedThreshold={0.3}
-    />
+    <View style={{ marginTop: 20, padding: 12 }}>
+      <TextInput
+        placeholder="Search Users...."
+        style={{
+          borderWidth: 1,
+          borderRadius: 8,
+          padding: 12,
+          marginBottom: 10,
+        }}
+        onChangeText={setQ}
+        value={q}
+      />
+      <FlatList
+        data={data}
+        keyExtractor={x => String(x.id)}
+        renderItem={({ item }) => (
+          <Text style={{ padding: 12 }}>
+            {item.id} : {item.name}
+          </Text>
+        )}
+      />
+    </View>
   );
 }
 
